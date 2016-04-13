e08ab505eada9323f04dd711828414cac54a5cbf
hilllo <hilllocanton@gmail.com>
2016-04-13 09:50:08 -0700

    asst4 initial

---
+++ asst4_finalproject/src/sampler.cpp
@@ -0,0 +1,75 @@
+#include "sampler.h"
+
+namespace CMU462 {
+
+// Uniform Sampler2D Implementation //
+
+Vector2D UniformGridSampler2D::get_sample() const {
+
+  // TODO:
+  // Implement uniform 2D grid sampler
+
+  double Xi1 = (double)(std::rand()) / RAND_MAX;
+  double Xi2 = (double)(std::rand()) / RAND_MAX;
+
+  return Vector2D(Xi1,Xi2);
+  // return Vector2D(0.5,0.5);
+
+}
+
+// Uniform Hemisphere Sampler3D Implementation //
+
+Vector3D UniformHemisphereSampler3D::get_sample() const {
+
+  double Xi1 = (double)(std::rand()) / RAND_MAX;
+  double Xi2 = (double)(std::rand()) / RAND_MAX;
+
+  double theta = acos(Xi1);
+  double phi = 2.0 * PI * Xi2;
+
+  double xs = sinf(theta) * cosf(phi);
+  double ys = sinf(theta) * sinf(phi);
+  double zs = cosf(theta);
+
+  return Vector3D(xs, ys, zs);
+
+}
+
+Vector3D CosineWeightedHemisphereSampler3D::get_sample() const {
+  float f;
+  return get_sample(&f);
+}
+
+Vector3D CosineWeightedHemisphereSampler3D::get_sample(float *pdf) const {
+  // You may implement this, but don't have to.
+  double Xi1 = (double)(std::rand()) / RAND_MAX; // Latitude
+  double Xi2 = (double)(std::rand()) / RAND_MAX; // Longtitude
+
+  double theta = acos(sqrt(1.0-Xi1));
+  double phi = 2.0 * PI * Xi2;
+
+  double xs = sin(theta) * cos(phi);
+  double ys = cos(theta);
+  double zs = sin(theta) * sin(phi);
+
+  Vector3D y = Vector3D(0.0, 0.0, 1.0);
+  Vector3D h = y;
+  if (fabs(h.x)<=fabs(h.y) && fabs(h.x)<=fabs(h.z))
+      h.x= 1.0;
+  else if (fabs(h.y)<=fabs(h.x) && fabs(h.y)<=fabs(h.z))
+      h.y= 1.0;
+  else
+      h.z= 1.0;
+
+  Vector3D x = cross(h,y).unit();
+  Vector3D z = cross(x,y).unit();
+
+  Vector3D direction = xs * x + ys * y + zs * z;
+
+  *pdf = cos(theta) / PI;
+  return direction.unit();
+  // return Vector3D(0, 0, 1);
+}
+
+
+} // namespace CMU462
---
+++ asst4_finalproject/src/static_scene/triangle.cpp
@@ -0,0 +1,162 @@
+#include "triangle.h"
+
+#include "CMU462/CMU462.h"
+#include "GL/glew.h"
+
+namespace CMU462 { namespace StaticScene {
+
+Triangle::Triangle(const Mesh* mesh, size_t v1, size_t v2, size_t v3) :
+    mesh(mesh), v1(v1), v2(v2), v3(v3) { }
+
+template <typename T>
+const T min3(const T a, const T b, const T c)
+{
+  return std::min(a, std::min(b, c));
+}
+
+template <typename T>
+const T max3(const T a, const T b, const T c)
+{
+  return std::max(a, std::max(b, c));
+}
+
+BBox Triangle::get_bbox() const {
+
+  // TODO:
+  // compute the bounding box of the triangle
+  Vector3D p0 = mesh->positions[v1];
+  Vector3D p1 = mesh->positions[v2];
+  Vector3D p2 = mesh->positions[v3];
+
+  Vector3D min,max;
+
+  min.x = min3(p0.x,p1.x,p2.x);
+  min.y = min3(p0.y,p1.y,p2.y);
+  min.z = min3(p0.z,p1.z,p2.z);
+
+  max.x = max3(p0.x,p1.x,p2.x);
+  max.y = max3(p0.y,p1.y,p2.y);
+  max.z = max3(p0.z,p1.z,p2.z);
+  //
+  return BBox(min,max);
+}
+
+bool Triangle::intersect(const Ray& r) const {
+
+  // TODO: implement ray-triangle intersection
+  // std::cout<<"Start intersecting!"<<std::endl;
+  Vector3D p0 = mesh->positions[v1];
+  Vector3D p1 = mesh->positions[v2];
+  Vector3D p2 = mesh->positions[v3];
+  Vector3D e1 = p1 - p0;
+  Vector3D e2 = p2 - p0;
+  Vector3D s = r.o - p0;
+  Vector3D d = r.d;
+
+  Vector3D e1xd = cross(e1,d);
+  Vector3D sxe2 = cross(s,e2);
+
+  double den = dot(e1xd,e2);
+  if(den == 0.0) {
+    return false;
+  }
+
+  double u = dot(sxe2,d) / den;
+  double v = dot(e1xd,s) / den;
+  double t = dot(sxe2,e1) / den;
+  if(u>0 && v>0 && u+v<1 && t>=r.min_t && t <= r.max_t){
+    // std::cout<<"true!";
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+bool Triangle::intersect(const Ray& r, Intersection *isect) const {
+
+  // TODO:
+  // implement ray-triangle intersection. When an intersection takes
+  // place, the Intersection data should be updated accordingly
+  // std::cout<<"Start intersecting 2!"<<std::endl;
+  {
+    Vector3D p0 = mesh->positions[v1];
+    Vector3D p1 = mesh->positions[v2];
+    Vector3D p2 = mesh->positions[v3];
+    Vector3D e1 = p1 - p0;
+    Vector3D e2 = p2 - p0;
+
+    Vector3D s = r.o - p0;
+    Vector3D d = r.d;
+
+    Vector3D e1xd = cross(e1,d);
+    Vector3D sxe2 = cross(s,e2);
+
+    double den = dot(e1xd,e2);
+    if(den == 0.0) {
+      return false;
+    }
+
+    double u = - dot(sxe2,d) / den;
+    double v = dot(e1xd,s) / den;
+    double t = - dot(sxe2,e1) / den;
+
+    if(u>0 && v>0 && u+v<1 && t>=r.min_t && t <= r.max_t){
+      // std::cout<<"true!";
+      // t: the ray's t-value of the hit point
+      isect->t = t;
+
+      // primitive: a pointer to the primitive that was hit
+      isect->primitive = this;
+
+      // bsdf: a pointer to the surface bsdf at the hit point (obtained via mesh->get_bsdf())
+      isect->bsdf = mesh->get_bsdf();
+
+      // n: the normal of the surface at the hit point.
+      isect->n = (1-u-v) * mesh->normals[v1] + u*mesh->normals[v2] + v*mesh->normals[v3];
+      isect->n.normalize();
+      if (dot(isect->n, d) > 0) {
+        isect->n *= -1;
+      }
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+}
+
+void Triangle::draw(const Color& c) const {
+  glColor4f(c.r, c.g, c.b, c.a);
+  glBegin(GL_TRIANGLES);
+  glVertex3d(mesh->positions[v1].x,
+             mesh->positions[v1].y,
+             mesh->positions[v1].z);
+  glVertex3d(mesh->positions[v2].x,
+             mesh->positions[v2].y,
+             mesh->positions[v2].z);
+  glVertex3d(mesh->positions[v3].x,
+             mesh->positions[v3].y,
+             mesh->positions[v3].z);
+  glEnd();
+}
+
+void Triangle::drawOutline(const Color& c) const {
+  glColor4f(c.r, c.g, c.b, c.a);
+  glBegin(GL_LINE_LOOP);
+  glVertex3d(mesh->positions[v1].x,
+             mesh->positions[v1].y,
+             mesh->positions[v1].z);
+  glVertex3d(mesh->positions[v2].x,
+             mesh->positions[v2].y,
+             mesh->positions[v2].z);
+  glVertex3d(mesh->positions[v3].x,
+             mesh->positions[v3].y,
+             mesh->positions[v3].z);
+  glEnd();
+}
+
+
+
+} // namespace StaticScene
+} // namespace CMU462
